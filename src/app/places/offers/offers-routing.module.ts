ba6aa7aa46c5280576d97f77851c521a70c46a03
SyedAbdullah-2001 <syedabdullahhusaini2001@gmail.com>
2021-08-09 19:58:27 +0500

    update tabs

---
+++ src/app/places/offers/offers-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { OffersPage } from './offers.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: OffersPage
+  },
+  {
+    path: 'new-offers',
+    loadChildren: () => import('./new-offers/new-offers.module').then( m => m.NewOffersPageModule)
+  },
+  {
+    path: 'edit-offers',
+    loadChildren: () => import('./edit-offers/edit-offers.module').then( m => m.EditOffersPageModule)
+  },
+  {
+    path: 'offer-bookings',
+    loadChildren: () => import('./offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class OffersPageRoutingModule {}
